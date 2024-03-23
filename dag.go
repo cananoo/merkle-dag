18ef41768626eec3f200e1b1b01b9be739e0ad2b
root <2059997196@qq.com>
2024-03-23 13:28:37 +0800

    dag2file

--- dag.go
+++ dag.go
@@ -5,7 +5,16 @@ import (
 	"hash"
 )
 
-const LIMIT = 4096
+const (
+	LIST_LIMIT  = 2048
+	BLOCK_LIMIT = 256 * 1024
+)
+
+const (
+	BLOB = "blob"
+	LIST = "list"
+	TREE = "tree"
+)
 
 type Link struct {
 	Name string
@@ -18,122 +27,151 @@ type Object struct {
 	Data  []byte
 }
 
-func dfsForSliceFile(hight int, node File, store KVStore, seedId int, h hash.Hash) (*Object, int) {
-	if hight == 1 {
-		if (len(node.Bytes()) - seedId) <= 256*1024 {
-			data := node.Bytes()[seedId:]
-			blob := Object{
-				Links: nil,
-				Data:  data,
-			}
-			jsonMarshal, _ := json.Marshal(blob)
-			h.Reset()
-			h.Write(jsonMarshal)
-			flag, _ := store.Has(h.Sum(nil))
-			if !flag {
-				store.Put(h.Sum(nil), data)
-			}
-			return &blob, len(data)
-		}
-		links := &Object{}
-		lenData := 0
-		for i := 1; i <= LIMIT; i++ {
-			end := seedId + 256*1024
-			if len(node.Bytes()) < end {
-				end = len(node.Bytes())
-			}
-			data := node.Bytes()[seedId:end]
-			blob := Object{
-				Links: nil,
-				Data:  data,
-			}
-			lenData += len(data)
-			jsonMarshal, _ := json.Marshal(blob)
-			h.Reset()
-			h.Write(jsonMarshal)
-			flag, _ := store.Has(h.Sum(nil))
-			if !flag {
-				store.Put(h.Sum(nil), data)
-			}
-			links.Links = append(links.Links, Link{
-				Hash: h.Sum(nil),
-				Size: len(data),
-			})
-			links.Data = append(links.Data, []byte("blob")...)
-			seedId += 256 * 1024
-			if seedId >= len(node.Bytes()) {
-				break
-			}
-		}
-		jsonMarshal, _ := json.Marshal(links)
-		h.Reset()
-		h.Write(jsonMarshal)
-		flag, _ := store.Has(h.Sum(nil))
-		if !flag {
-			store.Put(h.Sum(nil), jsonMarshal)
-		}
-		return links, lenData
-	} else {
-		links := &Object{}
-		lenData := 0
-		for i := 1; i <= LIMIT; i++ {
-			if seedId >= len(node.Bytes()) {
-				break
-			}
-			tmp, lens := dfsForSliceFile(hight-1, node, store, seedId, h)
-			lenData += lens
-			jsonMarshal, _ := json.Marshal(tmp)
-			h.Reset()
-			h.Write(jsonMarshal)
-			links.Links = append(links.Links, Link{
-				Hash: h.Sum(nil),
-				Size: lens,
-			})
-			typeName := "link"
-			if tmp.Links == nil {
-				typeName = "blob"
-			}
-			links.Data = append(links.Data, []byte(typeName)...)
-		}
-		jsonMarshal, _ := json.Marshal(links)
-		h.Reset()
-		h.Write(jsonMarshal)
-		flag, _ := store.Has(h.Sum(nil))
-		if !flag {
-			store.Put(h.Sum(nil), jsonMarshal)
-		}
-		return links, lenData
+func (obj *Object) appendActionAsTree(key []byte, length int, childObjName string, childObjType string) {
+	obj.Links = append(obj.Links, Link{
+		Hash: key,
+		Size: length,
+		Name: childObjName,
+	})
+	obj.Data = append(obj.Data, []byte(childObjType)...)
+}
+
+func (obj *Object) appendActionAsList(key []byte, length int, childObjType string) {
+	obj.appendActionAsTree(key, length, "", childObjType)
+}
+
+func Add(store KVStore, node Node, h hash.Hash) []byte {
+	// TODO 将分片写入到KVStore中，并返回Merkle Root
+	switch node.Type() {
+	case FILE:
+		file := node.(File)
+		root := sliceFile(file, store, h)
+		return getKey(root, h)
+	case DIR:
+		dir := node.(Dir)
+		root := sliceDir(dir, store, h)
+		return getKey(root, h)
+	}
+	return nil
+}
+
+func getKey(obj *Object, h hash.Hash) []byte {
+	key, _ := getKeyAndValue(obj, h)
+	return key
+}
+
+func getKeyAndValue(obj *Object, h hash.Hash) ([]byte, []byte) {
+	jsonMarshal, _ := json.Marshal(obj)
+	h.Reset()
+	h.Write(jsonMarshal)
+	return h.Sum(nil), jsonMarshal
+}
+
+func put(store KVStore, key []byte, value []byte, objType string) {
+	if objType != TREE && len(value) > BLOCK_LIMIT {
+		panic("block over the limit")
+	}
+	store.Put(key, value)
+}
+
+func saveObject(obj *Object, h hash.Hash, store KVStore, objType string) {
+	key, value := getKeyAndValue(obj, h)
+	flag, _ := store.Has(key)
+	if !flag {
+		put(store, key, value, objType)
 	}
 }
-func sliceFile(node File, store KVStore, h hash.Hash) *Object {
-	// fmt.Println("222222")
-	if len(node.Bytes()) <= 256*1024 {
-		data := node.Bytes()
-		blob := Object{
-			Links: nil,
-			Data:  data,
-		}
-		jsonMarshal, _ := json.Marshal(blob)
-		h.Reset()
-		h.Write(jsonMarshal)
-		flag, _ := store.Has(h.Sum(nil))
-		if !flag {
-			store.Put(h.Sum(nil), data)
-		}
-		return &blob
+
+func saveBlob(blob *Object, h hash.Hash, store KVStore) {
+	key := getKey(blob, h)
+	flag, _ := store.Has(key)
+	if !flag {
+		put(store, key, blob.Data, BLOB)
 	}
-	linkLen := (len(node.Bytes()) + (256*1024 - 1)) / (256 * 1024)
+}
+
+func newBlob(data []byte, h hash.Hash, store KVStore) *Object {
+	blob := &Object{
+		Links: nil,
+		Data:  data,
+	}
+	saveBlob(blob, h, store)
+	return blob
+}
+
+func checkObjIsBlobOrList(obj *Object) string {
+	res := LIST
+	if obj.Links == nil {
+		res = BLOB
+	}
+	return res
+}
+func sliceFile(node File, store KVStore, h hash.Hash) *Object {
+	nodeData := node.Bytes()
+	nodeLen := len(nodeData)
+	if nodeLen <= BLOCK_LIMIT {
+		return newBlob(nodeData, h, store)
+	}
+	//list
+	linkLen := (nodeLen + (BLOCK_LIMIT - 1)) / BLOCK_LIMIT
 	hight := 0
 	tmp := linkLen
 	for {
 		hight++
-		tmp /= LIMIT
+		tmp /= LIST_LIMIT
 		if tmp == 0 {
 			break
 		}
 	}
-	res, _ := dfsForSliceFile(hight, node, store, 0, h)
+	seedId := 0
+	res, _ := dfsForSliceList(hight, node, store, &seedId, h)
 	return res
+}
+
+func dfsForSliceList(hight int, node File, store KVStore, seedId *int, h hash.Hash) (*Object, int) {
+	if hight == 1 {
+		return unionBlob(node, store, seedId, h)
+	} else { // > 1 depth list
+		list := &Object{}
+		lenData := 0
+		for i := 1; i <= LIST_LIMIT && *seedId < len(node.Bytes()); i++ {
+			tmp, lens := dfsForSliceList(hight-1, node, store, seedId, h)
+			lenData += lens
+			key := getKey(tmp, h)
+			typeName := checkObjIsBlobOrList(tmp)
+			list.appendActionAsList(key, lens, typeName)
+		}
+		saveObject(list, h, store, LIST)
+		return list, lenData
+	}
+}
+
+func unionBlob(node File, store KVStore, seedId *int, h hash.Hash) (*Object, int) {
+	// only 1 blob
+	nodeData := node.Bytes()
+	nodeLen := len(nodeData)
+	if (nodeLen - *seedId) <= BLOCK_LIMIT {
+		data := nodeData[*seedId:]
+		return newBlob(data, h, store), len(data)
+	}
+	// > 1 blob
+	list := &Object{}
+	lenData := 0
+	for i := 1; i <= LIST_LIMIT && *seedId < nodeLen; i++ {
+		end := *seedId + BLOCK_LIMIT
+		if nodeLen < end {
+			end = nodeLen
+		}
+		data := nodeData[*seedId:end]
+		blob := newBlob(data, h, store)
+		lenBlob := len(data)
+		lenData += lenBlob
+		key := getKey(blob, h)
+		list.appendActionAsList(key, lenBlob, BLOB)
+		*seedId += BLOCK_LIMIT
+	}
+	saveObject(list, h, store, LIST)
+	return list, lenData
 }
 
 func sliceDir(node Dir, store KVStore, h hash.Hash) *Object {
@@ -141,59 +179,22 @@ func sliceDir(node Dir, store KVStore, h hash.Hash) *Object {
 	treeObject := &Object{}
 	for iter.Next() {
 		node := iter.Node()
-		if node.Type() == FILE {
+		switch node.Type() {
+		case FILE:
 			file := node.(File)
 			tmp := sliceFile(file, store, h)
-			jsonMarshal, _ := json.Marshal(tmp)
-			h.Reset()
-			h.Write(jsonMarshal)
-			treeObject.Links = append(treeObject.Links, Link{
-				Hash: h.Sum(nil),
-				Size: int(file.Size()),
-				Name: file.Name(),
-			})
-			typeName := "link"
-			if tmp.Links == nil {
-				typeName = "blob"
-			}
-			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
-		} else {
+			key := getKey(tmp, h)
+			typeName := checkObjIsBlobOrList(tmp)
+			treeObject.appendActionAsTree(key, int(file.Size()), file.Name(), typeName)
+
+		case DIR:
 			dir := node.(Dir)
 			tmp := sliceDir(dir, store, h)
-			jsonMarshal, _ := json.Marshal(tmp)
-			h.Reset()
-			h.Write(jsonMarshal)
-			treeObject.Links = append(treeObject.Links, Link{
-				Hash: h.Sum(nil),
-				Size: int(dir.Size()),
-				Name: dir.Name(),
-			})
-			typeName := "tree"
-			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+			key := getKey(tmp, h)
+			typeName := TREE
+			treeObject.appendActionAsTree(key, int(dir.Size()), dir.Name(), typeName)
 		}
 	}
-	jsonMarshal, _ := json.Marshal(treeObject)
-	h.Reset()
-	h.Write(jsonMarshal)
-	flag, _ := store.Has(h.Sum(nil))
-	if !flag {
-		store.Put(h.Sum(nil), jsonMarshal)
-	}
+	saveObject(treeObject, h, store, TREE)
 	return treeObject
-}
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	if node.Type() == FILE {
-		file := node.(File)
-		tmp := sliceFile(file, store, h)
-		jsonMarshal, _ := json.Marshal(tmp)
-		h.Write(jsonMarshal)
-		return h.Sum(nil)
-	} else {
-		dir := node.(Dir)
-		tmp := sliceDir(dir, store, h)
-		jsonMarshal, _ := json.Marshal(tmp)
-		h.Write(jsonMarshal)
-		return h.Sum(nil)
-	}
 }
--- dag2file.go
+++ dag2file.go
@@ -5,100 +5,78 @@ import (
 	"strings"
 )
 
+const STEP = 4
+
 // Hash to file
 
 // example a path : /doc/tmp/temp.txt
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
+	// 根据hash和path， 返curObjBinary回对应的文件, hash对应的类型是tree
 	flag, _ := store.Has(hash)
 	if flag {
 		objBinary, _ := store.Get(hash)
-		var obj Object
-		json.Unmarshal(objBinary, &obj)
+		obj := binaryToObj(objBinary)
 		pathArr := strings.Split(path, "/")
-		cur := 0
-		if cur >= len(pathArr) {
-			return nil
+		cur := 1
+		return getFileByDir(obj, pathArr, cur, store)
+	}
+	return nil
+}
+
+func getFileByDir(obj *Object, pathArr []string, cur int, store KVStore) []byte {
+	if cur >= len(pathArr) {
+		return nil
+	}
+	index := 0
+	for i := range obj.Links {
+		objType := string(obj.Data[index : index+STEP])
+		index += STEP
+		objInfo := obj.Links[i]
+		if objInfo.Name != pathArr[cur] {
+			continue
 		}
-		index := 0
-		for i := range obj.Links {
-			objType := string(obj.Data[index : index+4])
-			index += 4
-			objInfo := obj.Links[i]
-			if objType == "tree" {
-				objDirBinary, _ := store.Get(objInfo.Hash)
-				var objDir Object
-				json.Unmarshal(objDirBinary, &objDir)
-				ans := getFileByDir(objDir, pathArr, cur+1, store)
+		switch objType {
+		case TREE:
+			objDirBinary, _ := store.Get(objInfo.Hash)
+			objDir := binaryToObj(objDirBinary)
+			ans := getFileByDir(objDir, pathArr, cur+1, store)
+			if ans != nil {
 				return ans
-			} else if objType == "blob" {
-				if objInfo.Name == pathArr[cur] {
-					ans, _ := store.Get(objInfo.Hash)
-					return ans
-				}
-			} else {
-				if objInfo.Name == pathArr[cur] {
-					objLinkBinary, _ := store.Get(objInfo.Hash)
-					var objLink Object
-					json.Unmarshal(objLinkBinary, &objLink)
-					ans := getFileByLink(objLink, store)
-					return ans
-				}
 			}
+		case BLOB:
+			ans, _ := store.Get(objInfo.Hash)
+			return ans
+		case LIST:
+			objLinkBinary, _ := store.Get(objInfo.Hash)
+			objList := binaryToObj(objLinkBinary)
+			ans := getFileByList(objList, store)
+			return ans
 		}
 	}
 	return nil
 }
 
-func getFileByLink(obj Object, store KVStore) []byte {
+func getFileByList(obj *Object, store KVStore) []byte {
 	ans := make([]byte, 0)
 	index := 0
 	for i := range obj.Links {
-		curObjType := string(obj.Data[index : index+4])
-		index += 4
+		curObjType := string(obj.Data[index : index+STEP])
+		index += STEP
 		curObjLink := obj.Links[i]
 		curObjBinary, _ := store.Get(curObjLink.Hash)
-		var curObj Object
-		json.Unmarshal(curObjBinary, &curObj)
-		if curObjType == "blob" {
+		curObj := binaryToObj(curObjBinary)
+		if curObjType == BLOB {
 			ans = append(ans, curObjBinary...)
-		} else {
-			tmp := getFileByLink(curObj, store)
+		} else { //List
+			tmp := getFileByList(curObj, store)
 			ans = append(ans, tmp...)
 		}
 	}
 	return ans
 }
 
-func getFileByDir(obj Object, pathArr []string, cur int, store KVStore) []byte {
-	if cur >= len(pathArr) {
-		return nil
-	}
-	index := 0
-	for i := range obj.Links {
-		objType := string(obj.Data[index : index+4])
-		index += 4
-		objInfo := obj.Links[i]
-		if objType == "tree" {
-			objDirBinary, _ := store.Get(objInfo.Hash)
-			var objDir Object
-			json.Unmarshal(objDirBinary, &objDir)
-			ans := getFileByDir(objDir, pathArr, cur+1, store)
-			return ans
-		} else if objType == "blob" {
-			if objInfo.Name == pathArr[cur] {
-				ans, _ := store.Get(objInfo.Hash)
-				return ans
-			}
-		} else {
-			if objInfo.Name == pathArr[cur] {
-				objLinkBinary, _ := store.Get(objInfo.Hash)
-				var objLink Object
-				json.Unmarshal(objLinkBinary, &objLink)
-				ans := getFileByLink(objLink, store)
-				return ans
-			}
-		}
-	}
-	return nil
+func binaryToObj(objBinary []byte) *Object {
+	var res Object
+	json.Unmarshal(objBinary, &res)
+	return &res
 }
